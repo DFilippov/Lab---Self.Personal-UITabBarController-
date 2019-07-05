9d23ee869fcccf4ce2f45c206e35a650465b64c7
Ayu Filippova <ayufilippova@imac-ayu>
2019-07-06 00:49:39 +0300

    1

---
+++ Lab - Self.Personal (UITabBarController)/AppDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//  Lab - Self.Personal (UITabBarController)
+//
+//  Created by Ayu Filippova on 25.06.2019.
+//  Copyright © 2019 Dmitry Filippov. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+}
+
---
+++ Lab - Self.Personal (UITabBarController)/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Lab - Self.Personal (UITabBarController)
+//
+//  Created by Ayu Filippova on 25.06.2019.
+//  Copyright © 2019 Dmitry Filippov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
